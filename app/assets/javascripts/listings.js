4d1a429f38dfefaa5c602b68e6b44114f88e2c6b
Mark Lawrence <lawrence.ml@gmail.com>
2019-02-25 00:44:03 -0500

    add ajax and prototype listing

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,11 +1,19 @@
-$(function() {});
+$(function() {
+  getListings();
+});
 
 const getListings = () => {
   $.ajax({
     type: "get",
     url: "/listings",
     dataType: "json",
-    success: function(response) {}
+    success: function(response) {
+      response.forEach(listing => {
+        const newListing = new Listing(listing);
+        const newListingHtml = newListing.listingHTML();
+        document.getElementById("ajax-listings").innerHTML += newListingHtml;
+      });
+    }
   });
 };
 
@@ -20,5 +28,19 @@ class Listing {
     this.bedrooms = obj.bedrooms;
     this.bathrooms = obj.bathrooms;
     this.list_price = obj.list_price;
+    this.created_at = obj.created_at;
   }
 }
+
+Listing.prototype.listingHTML = function() {
+  return `
+    <ul>
+    <li>
+      Date Listed: ${this.created_at}<br>
+      Property Location:<br>
+      ${this.street}<br>
+      ${this.city}, ${this.state} ${this.zip_code}
+    </li>
+    </ul
+  `;
+};
