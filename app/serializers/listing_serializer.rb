50eea88ef4ad38671e01f2d5c5a7b90a9b1ce8e7
Mark Lawrence <lawrence.ml@gmail.com>
2019-02-25 00:37:31 -0500

    add created_at

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,3 +1,3 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :street, :city, :state, :zip_code, :property_type, :bedrooms, :bathrooms, :list_price
+  attributes :id, :street, :city, :state, :zip_code, :property_type, :bedrooms, :bathrooms, :list_price, :created_at
 end
