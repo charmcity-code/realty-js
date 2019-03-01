fc7d8844290554fa6f03f17bed851230d38a565c
Mark Lawrence <lawrence.ml@gmail.com>
2019-02-28 23:36:14 -0500

    add has_many relationships

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,3 +1,5 @@
 class ListingSerializer < ActiveModel::Serializer
   attributes :id, :street, :city, :state, :zip_code, :property_type, :bedrooms, :bathrooms, :list_price, :created_at
+  has_many :appointments
+  has_many :buyers, through: :appointments
 end
