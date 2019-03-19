6acc07ee5a7d095667e757cf9ed5eee4f58c0bec
Mark Lawrence <lawrence.ml@gmail.com>
2019-03-19 18:09:28 -0400

    remove unnecessary through: :appointments

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,5 @@
 class ListingSerializer < ActiveModel::Serializer
   attributes :id, :street, :city, :state, :zip_code, :property_type, :bedrooms, :bathrooms, :list_price, :created_at
   has_many :appointments
-  has_many :buyers, through: :appointments
+  has_many :buyers
 end
